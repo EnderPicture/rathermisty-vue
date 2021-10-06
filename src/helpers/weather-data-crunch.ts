f69922945a29b30972d6dbec367e1477ba263ced
Donny Wu <enderpicture@gmail.com>
2021-10-05 23:57:25 -0700

    completed data crunch

--- src/helpers/weather-data-crunch.ts
+++ src/helpers/weather-data-crunch.ts
@@ -1,99 +1,114 @@
+import { objectToString } from "@vue/shared";
 import {
   RawWeatherData,
   WeatherData,
   WeatherHour,
   WeatherDay,
+  ValueRange,
+  Hourly,
+  HourlyUnits,
+  HourlyData,
+  LooseObject,
+  Tense,
+  DailyUnits,
+  DailyData,
 } from "../interfaces/Types";
 
-export const crunchWeatherData = (rawWeatherData: RawWeatherData) => {
+export const crunchWeatherData = (
+  rawWeatherData: RawWeatherData
+): WeatherData => {
   let days: WeatherDay[] = [];
+  let dayIndex = -1;
 
-  // global max and min
-  let maxTemp = Number.MIN_VALUE;
-  let minTemp = Number.MAX_VALUE;
-  let minApparentTemp = Number.MIN_VALUE;
-  let maxApparentTemp = Number.MAX_VALUE;
-
-  let lastWeatherCode = -1;
-  let lastDay = -1;
-
-  let now = Date.now();
-
-  for (let i = 0; i < rawWeatherData.hourly.time.length; i++) {
-    // set up day;
-    let time = new Date(rawWeatherData.hourly.time[i] * 1000);
-    let weatherCode = rawWeatherData.hourly.weathercode[i];
-
-    // check if hour is a new day
-    let newDay = false;
-    if (time.getDate() !== lastDay) {
-      newDay = true;
-
-      let tense = "future";
-      let tomorrow = new Date();
-      tomorrow.setDate(time.getDate() + 7);
-      if (time.getTime() < now && tomorrow.getTime() > now) {
-        tense = "now";
-      } else if (time.getTime() < now) {
-        tense = "past";
-      }
-      days.push({
-        tense: tense,
-        minTemp: 0,
-        maxTemp: 0,
-        minApparentTemp: 0,
-        maxApparentTemp: 0,
-        hours: [],
-      });
-      lastDay = time.getDate();
-    }
-
-    let newWeather = false;
-    if (weatherCode != lastWeatherCode) {
-      newWeather = true;
-      lastWeatherCode = weatherCode;
-    }
-
-    let tense = "future";
-    if (now - 60 * 60 * 1000 > time.getTime()) {
-      tense = "past";
-    } else if (now > time.getTime()) {
-      tense = "now";
-    }
-
-    let hour: WeatherHour = {
-      tempUnit: rawWeatherData.hourly_units.apparent_temperature,
-      precipitationUnit: rawWeatherData.hourly_units.precipitation,
-      apparentTemp: rawWeatherData.hourly.apparent_temperature[i],
-      temp: rawWeatherData.hourly.temperature_2m[i],
-      cloudCover: rawWeatherData.hourly.cloudcover[i],
-      dewPoint: rawWeatherData.hourly.dewpoint_2m[i],
-      relativeHumidity: rawWeatherData.hourly.relativehumidity_2m[i],
-      windSpeed: rawWeatherData.hourly.windspeed_10m[i],
-      precipitation: rawWeatherData.hourly.precipitation[i],
-
-      weatherCode: weatherCode,
-
-      time: time,
-      newDay: newDay,
-      newWeather: newWeather,
-      tense: tense,
-    };
-
-    if (hour.temp > maxTemp) maxTemp = hour.temp;
-    if (hour.temp < minTemp) minTemp = hour.temp;
-    if (hour.temp > maxApparentTemp) maxTemp = hour.apparentTemp;
-    if (hour.temp < minApparentTemp) minTemp = hour.apparentTemp;
-
-    days[days.length - 1].hours.push(hour);
-  }
-  console.log(minTemp, maxTemp);
-
-  const outcome: WeatherData = {
-    days: days,
-    maxTemp: maxTemp,
-    minTemp: minTemp,
+  let tempRange: ValueRange = {
+    min: Number.MAX_VALUE,
+    max: Number.MIN_VALUE,
+  };
+  let apparentTempRange: ValueRange = {
+    min: Number.MAX_VALUE,
+    max: Number.MIN_VALUE,
   };
 
-  return outcome;
+  let lastHour: WeatherHour | null = null;
+  let lastDay: WeatherDay | null = null;
+
+  let now = new Date();
+
+  // loop though every hour
+  for (let i = 0; i < rawWeatherData.hourly.time.length; i++) {
+    const hour: WeatherHour = {
+      date: new Date(rawWeatherData.hourly.time[i]),
+      tense: "past",
+      units: rollup(-1, rawWeatherData.hourly_units) as HourlyUnits,
+      values: rollup(i, rawWeatherData.hourly) as HourlyData,
+    };
+    hour.tense = computeTense(hour.date, 1);
+
+    // new day
+    if (lastHour?.date.getDate() !== hour.date.getDate()) {
+      dayIndex++;
+      const day: WeatherDay = {
+        date: new Date(hour.date.toDateString()),
+        tense: "now",
+        units: rollup(-1, rawWeatherData.daily_units) as DailyUnits,
+        values: rollup(dayIndex, rawWeatherData.daily) as DailyData,
+        hours: [],
+      };
+      day.tense = computeTense(day.date, 24);
+
+      // calculate minMax
+      if (tempRange.min > day.values.temperature_2m_min)
+        tempRange.min = day.values.temperature_2m_min;
+      if (tempRange.max < day.values.temperature_2m_max)
+        tempRange.max = day.values.temperature_2m_max;
+      if (apparentTempRange.min > day.values.apparent_temperature_min)
+        apparentTempRange.min = day.values.apparent_temperature_min;
+      if (apparentTempRange.max < day.values.apparent_temperature_max)
+        apparentTempRange.max = day.values.apparent_temperature_max;
+
+      days.push(day);
+      lastDay = day;
+    }
+    lastDay?.hours.push(hour);
+    lastHour = hour;
+  }
+
+  return {
+    days: days,
+
+    tempRange: tempRange,
+    apparentTempRange: apparentTempRange,
+  };
 };
+
+const computeTense = (date: Date, precision: 1 | 24): Tense => {
+  let tense: Tense = "past";
+
+  let now = new Date();
+
+  let future = new Date(date.getTime());
+  future.setHours(future.getHours() + precision);
+
+  if (+date < +now && +now < +future) {
+    tense = "now";
+  } else if (+now < +date) {
+    tense = "future";
+  }
+
+  return tense;
+};
+
+const rollup = (index: number, object: LooseObject): LooseObject => {
+  const keys = Object.keys(object);
+  const output: LooseObject = {};
+  keys.forEach((key) => {
+    if (index === -1) {
+      output[key] = object[key];
+    } else {
+      output[key] = object[key][index];
+    }
+  });
+  return output;
+};
+
+const insertValueInRange = (value: number, range: ValueRange) => {};
--- src/interfaces/Types.ts
+++ src/interfaces/Types.ts
@@ -1,74 +1,217 @@
 export interface RawWeatherData {
-  hourly: Hourly;
-  hourly_units: HourlyUnits;
   generationtime_ms: number;
+  latitude: number;
+  daily_units: DailyUnits;
+  hourly: Hourly;
   utc_offset_seconds: number;
   elevation: number;
-  latitude: number;
   longitude: number;
-  current_weather: CurrentWeather;
+  hourly_units: HourlyUnits;
+  daily: Daily;
 }
 
-export interface CurrentWeather {
-  winddirection: number;
-  weathercode: number;
-  temperature: number;
-  time: number;
-  windspeed: number;
-}
-
-export interface Hourly {
-  apparent_temperature: number[];
-  dewpoint_2m: number[];
-  windspeed_10m: number[];
-  time: number[];
-  precipitation: number[];
-  temperature_2m: number[];
-  relativehumidity_2m: number[];
-  cloudcover: number[];
+export interface Daily {
+  apparent_temperature_min: number[];
+  precipitation_sum: number[];
+  windgusts_10m_max: number[];
+  temperature_2m_max: number[];
+  winddirection_10m_dominant: number[];
+  precipitation_hours: number[];
+  shortwave_radiation_sum: number[];
+  apparent_temperature_max: number[];
+  windspeed_10m_max: number[];
+  temperature_2m_min: number[];
+  time: string[];
+  sunset: string[];
+  sunrise: string[];
   weathercode: number[];
 }
 
-export interface HourlyUnits {
-  apparent_temperature: string;
-  dewpoint_2m: string;
-  windspeed_10m: string;
-  temperature_2m: string;
+export interface DailyUnits {
+  precipitation_sum: string;
+  apparent_temperature_min: string;
+  windgusts_10m_max: string;
+  temperature_2m_max: string;
+  winddirection_10m_dominant: string;
+  precipitation_hours: string;
+  shortwave_radiation_sum: string;
+  apparent_temperature_max: string;
+  windspeed_10m_max: string;
+  temperature_2m_min: string;
   time: string;
-  precipitation: string;
-  relativehumidity_2m: string;
-  cloudcover: string;
+  sunset: string;
+  sunrise: string;
   weathercode: string;
 }
 
+export interface Hourly {
+  temperature_2m: number[];
+  pressure_msl: number[];
+  soil_temperature_54cm: number[];
+  windspeed_80m: number[];
+  precipitation: number[];
+  winddirection_120m: number[];
+  apparent_temperature: number[];
+  winddirection_10m: number[];
+  soil_moisture_3_9cm: number[];
+  freezinglevel_height: number[];
+  winddirection_180m: number[];
+  snow_height: number[];
+  windspeed_120m: number[];
+  soil_temperature_18cm: number[];
+  direct_radiation: number[];
+  windgusts_10m: number[];
+  evapotranspiration: number[];
+  soil_moisture_1_3cm: number[];
+  cloudcover: number[];
+  shortwave_radiation: number[];
+  relativehumidity_2m: number[];
+  weathercode: number[];
+  vapor_pressure_deficit: number[];
+  soil_temperature_6cm: number[];
+  soil_moisture_9_27cm: number[];
+  windspeed_10m: number[];
+  dewpoint_2m: number[];
+  winddirection_80m: number[];
+  soil_temperature_0cm: number[];
+  cloudcover_low: number[];
+  cloudcover_mid: number[];
+  soil_moisture_27_81cm: number[];
+  time: string[];
+  diffuse_radiation: number[];
+  windspeed_180m: number[];
+  cloudcover_high: number[];
+  soil_moisture_0_1cm: number[];
+}
+
+export interface HourlyUnits {
+  soil_moisture_0_1cm: string;
+  pressure_msl: string;
+  temperature_2m: string;
+  windspeed_80m: string;
+  precipitation: string;
+  freezinglevel_height: string;
+  apparent_temperature: string;
+  winddirection_10m: string;
+  winddirection_120m: string;
+  soil_moisture_3_9cm: string;
+  snow_height: string;
+  winddirection_180m: string;
+  windspeed_120m: string;
+  soil_temperature_18cm: string;
+  direct_radiation: string;
+  windgusts_10m: string;
+  soil_moisture_1_3cm: string;
+  evapotranspiration: string;
+  cloudcover: string;
+  shortwave_radiation: string;
+  vapor_pressure_deficit: string;
+  weathercode: string;
+  relativehumidity_2m: string;
+  soil_temperature_6cm: string;
+  soil_moisture_9_27cm: string;
+  windspeed_10m: string;
+  dewpoint_2m: string;
+  winddirection_80m: string;
+  soil_temperature_0cm: string;
+  cloudcover_low: string;
+  cloudcover_mid: string;
+  soil_moisture_27_81cm: string;
+  time: string;
+  diffuse_radiation: string;
+  windspeed_180m: string;
+  cloudcover_high: string;
+  soil_temperature_54cm: string;
+}
+
+export interface HourlyData {
+  temperature_2m: number;
+  pressure_msl: number;
+  soil_temperature_54cm: number;
+  windspeed_80m: number;
+  precipitation: number;
+  winddirection_120m: number;
+  apparent_temperature: number;
+  winddirection_10m: number;
+  soil_moisture_3_9cm: number;
+  freezinglevel_height: number;
+  winddirection_180m: number;
+  snow_height: number;
+  windspeed_120m: number;
+  soil_temperature_18cm: number;
+  direct_radiation: number;
+  windgusts_10m: number;
+  evapotranspiration: number;
+  soil_moisture_1_3cm: number;
+  cloudcover: number;
+  shortwave_radiation: number;
+  relativehumidity_2m: number;
+  weathercode: number;
+  vapor_pressure_deficit: number;
+  soil_temperature_6cm: number;
+  soil_moisture_9_27cm: number;
+  windspeed_10m: number;
+  dewpoint_2m: number;
+  winddirection_80m: number;
+  soil_temperature_0cm: number;
+  cloudcover_low: number;
+  cloudcover_mid: number;
+  soil_moisture_27_81cm: number;
+  time: string;
+  diffuse_radiation: number;
+  windspeed_180m: number;
+  cloudcover_high: number;
+  soil_moisture_0_1cm: number;
+}
+
+export interface DailyData {
+  apparent_temperature_min: number;
+  precipitation_sum: number;
+  windgusts_10m_max: number;
+  temperature_2m_max: number;
+  winddirection_10m_dominant: number;
+  precipitation_hours: number;
+  shortwave_radiation_sum: number;
+  apparent_temperature_max: number;
+  windspeed_10m_max: number;
+  temperature_2m_min: number;
+  time: string;
+  sunset: string;
+  sunrise: string;
+  weathercode: number;
+}
+
+export type Tense = "now" | "past" | "future";
+
+export interface WeatherHour {
+  tense: Tense;
+
+  date: Date;
+  units: HourlyUnits;
+  values: HourlyData;
+}
+
 export interface WeatherDay {
-  tense: string;
-  minTemp: number;
-  maxTemp: number;
-  minApparentTemp: number;
-  maxApparentTemp: number;
+  date: Date;
+  tense: Tense;
+
+  units: DailyUnits;
+  values: DailyData;
+
   hours: WeatherHour[];
 }
-export interface WeatherHour {
-  tempUnit: string;
-  apparentTemp: number;
-  temp: number;
-  weatherCode: number;
-  time: Date;
-  newDay: boolean;
-  newWeather: boolean;
-  tense: string;
-
-  cloudCover: number;
-  dewPoint: number;
-  relativeHumidity: number;
-  windSpeed: number;
-  precipitation: number;
-  precipitationUnit: string;
-}
-
 export interface WeatherData {
   days: WeatherDay[];
-  maxTemp: number;
-  minTemp: number;
+
+  tempRange: ValueRange;
+  apparentTempRange: ValueRange;
+}
+
+export interface ValueRange {
+  min: number;
+  max: number;
+}
+
+export interface LooseObject {
+  [key: string]: any;
 }
