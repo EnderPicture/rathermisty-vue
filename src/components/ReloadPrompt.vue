25df60d8a93e9548c13133aef9f6735b1834a5af
Donny Wu <enderpicture@gmail.com>
2021-11-14 23:32:36 -0800

    revert

--- src/components/ReloadPrompt.vue
+++ src/components/ReloadPrompt.vue
@@ -3,18 +3,14 @@ import { useRegisterSW } from "virtual:pwa-register/vue";
 
 const { offlineReady, needRefresh, updateServiceWorker } = useRegisterSW();
 
-if (needRefresh.value) {
-  updateServiceWorker();
-}
-
-// const close = async () => {
-//   offlineReady.value = false;
-//   needRefresh.value = false;
-// };
+const close = async () => {
+  offlineReady.value = false;
+  needRefresh.value = false;
+};
 </script>
 
 <template>
-  <!-- <div v-if="offlineReady || needRefresh" class="pwa-toast" role="alert">
+  <div v-if="offlineReady || needRefresh" class="pwa-toast" role="alert">
     <div class="message">
       <span v-if="offlineReady"> App ready to work offline </span>
       <span v-else>
@@ -23,7 +19,7 @@ if (needRefresh.value) {
     </div>
     <button v-if="needRefresh" @click="updateServiceWorker()">Reload</button>
     <button @click="close">Close</button>
-  </div> -->
+  </div>
 </template>
 
 <style>
